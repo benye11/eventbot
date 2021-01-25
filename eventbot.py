b99518461e4253969ad864ba66ed06b585e18866
Ben Ye <z_ye@ucsb.edu>
2021-01-25 00:25:58 -0800

    final fix hopefully

--- eventbot.py
+++ eventbot.py
@@ -42,7 +42,7 @@ class listener(commands.Cog):
     @commands.command()
     async def poll(self, ctx):
         #check if a poll exists. we should have only one
-        SQL = self.computesql(self.DATABASE_POLL_MESSAGE_ID_TABLE, "check_poll_message", "", str(ctx.id), "", "", "")
+        SQL = self.computesql(self.DATABASE_POLL_MESSAGE_ID_TABLE, "check_poll_message", "", str(ctx.channel.id), "", "", "")
         self.cur.execute(SQL)
         fetch = cur.fetchone()
         if fetch is not None:
