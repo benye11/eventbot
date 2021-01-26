bcbacc6e2e62cdf4fe7ac989740e74445158170d
Ben Ye <z_ye@ucsb.edu>
2021-01-25 21:02:28 -0800

    added a new command

--- eventbot.py
+++ eventbot.py
@@ -14,8 +14,6 @@ bot = commands.Bot(command_prefix='.')
 bot.remove_command('help')
 
 env_variables = (os.environ['DATABASE_URL'], os.environ['DATABASE_POLL_TABLE'], os.environ['DATABASE_POLL_MESSAGE_ID_TABLE'])
-for i in env_variables:
-    print('env: ', i)
 
 class listener(commands.Cog):
     def __init__(self, bot, env_variables):
@@ -36,7 +34,8 @@ class listener(commands.Cog):
         embed.add_field(name=".notify message", value="notify this channel with message", inline=False)
         embed.add_field(name=".schedule event_name event_day", value="schedules event notification at event_day\nevent_day format (year is assumed to be current year): [Format]: Month/Day Hour:Minute AM/PM\nexample: 01/23 4:30 PM", inline=False) #in the future, should be able to enter this into database. for now, just internal schedule
         #embed.add_field(name=".availability event_id", value="output people available for this event", inline=False) #implement later
-        embed.add_field(name=".availability event_day", value="output people available for this time\nweekday format: Monday or 1, Tuesday or 2, etc.", inline=False)
+        embed.add_field(name=".availability event_day", value="output people available for this time\nday format: Month/Day Example: 01/23", inline=False)
+        embed.add_field(name=".rr", value="output people haven't reacted to the poll. Alternate names: .requestresponse or .request_response")
         embed.add_field(name=".repo", value="output link to github repo. helpful links and resources are displayed in the README.md")
         embed.set_footer(text=datetime.now().strftime('%m/%d/%Y %I:%M %p'))
         await ctx.send(embed=embed)
@@ -44,12 +43,16 @@ class listener(commands.Cog):
     @commands.command() 
     async def poll(self, ctx):
         #check if a poll exists. we should have only one
-        SQL = self.computesql(self.DATABASE_POLL_MESSAGE_ID_TABLE, "check_poll_message", "", "", "'" + str(ctx.channel.id) + "'", "", "", self.args)
+        SQL = self.computesql(table=self.DATABASE_POLL_MESSAGE_ID_TABLE, action="check_poll_message_exists", channel_id="'" + str(ctx.channel.id) + "'")
         self.cur.execute(SQL)
         fetch = self.cur.fetchone()
-        if fetch is not None and fetch[1] == str(ctx.channel.id):
-            await ctx.send("poll already exists! please edit that one and don't delete it")
-        else:
+        try:
+            msg = await ctx.channel.fetch_message(int(fetch[0]))
+            if fetch is not None and fetch[1] == str(ctx.channel.id):
+                await ctx.send("poll already exists! please edit that one and don't delete it. It should be a pinned message")
+        except Exception as e: #message doesn't exist, then delete from db
+            SQL = self.computesql(table=self.DATABASE_POLL_MESSAGE_ID_TABLE, action="delete_poll_message", message_id=str(fetch[0]), channel_id=str(fetch[1]))
+        if fetch is None and fetch[1] == str(ctx.channel.id):
             embed = discord.Embed(color=0x32A852, title='Poll Weekly Availability anytime between 6-10PM', description="If you are available at least 30 mins between 6-10PM, please react")
             embed.set_author(name='Event Bot', icon_url='https://i.imgur.com/qn182DB.jpg')
             embed.set_thumbnail(url='https://image.flaticon.com/icons/png/512/1458/1458512.png')
@@ -57,7 +60,7 @@ class listener(commands.Cog):
             current_time = datetime.now().strftime('%m/%d/%Y %I:%M %p')
             embed.set_footer(text=current_time)
             message = await ctx.send(embed=embed)
-            SQL = self.computesql(self.DATABASE_POLL_MESSAGE_ID_TABLE, "set_poll_message", "", "'" + str(message.id) + "'", "'" + str(message.channel.id) + "'", 0, 0, self.args)
+            SQL = self.computesql(table=self.DATABASE_POLL_MESSAGE_ID_TABLE, action="set_poll_message", message_id="'" + str(message.id) + "'", channel_id="'" + str(message.channel.id) + "'")
             self.cur.execute(SQL)
             self.conn.commit() #commit changes to database
             embed.add_field(name= "Message ID", value=str(message.id), inline=False)
@@ -93,7 +96,7 @@ class listener(commands.Cog):
             #query sql
             day = datetime.strptime(arg + " " + str(datetime.now().year), "%m/%d %Y").strftime("%A").lower()
             #computesql(self, table, action, user_id, username, column, column_index, args):
-            SQL = self.computesql(self.DATABASE_POLL_TABLE, "fetch_users", "'" + str(ctx.channel.id) + "'", "", "", day, 0, self.args)
+            SQL = self.computesql(table=self.DATABASE_POLL_TABLE, action="fetch_available_users", channel_id="'" + str(ctx.channel.id) + "'", column=day)
             self.cur.execute(SQL)
             fetch = self.cur.fetchall()
             users = [x[0] for x in fetch]
@@ -107,6 +110,8 @@ class listener(commands.Cog):
                 await ctx.send(mentions[0] + " is available on " + arg + " (" + day + ")")
             else:
                 output = ', '.join(mentions)
+                index = output.rfind(',')
+                output = output[:index] + ' and' + output[index+1:]
                 await ctx.send(output + " are available on " + arg + " (" + day + ")")
             #now we have many tuples
             pass
@@ -116,6 +121,26 @@ class listener(commands.Cog):
             pass
         else:
             await ctx.send("[Usage]: .availability <event_id or event_day> [Error message]: invalid date or ID")
+
+    @commands.command(aliases=['requestresponse', 'request_response'])
+    async def rr(self, ctx):
+        SQL = self.computesql(table=self.DATABASE_POLL_TABLE, action="fetch_all_users", channel_id= "'" + str(ctx.channel.id) + "'")
+        self.cur.execute(SQL)
+        fetch = self.cur.fetchall()
+        user_ids = [x[0] for x in fetch]
+        mentions = []
+        for user_id in user_ids:
+            user = await self.bot.fetch_user(user_id)
+            mentions.append(user.mention)
+        if len(mentions) == 0:
+            await ctx.send("everyone has responded to the poll")
+        elif len(mentions) == 1:
+            await ctx.send(mentions[0] + " hasn't responded")
+        else:
+            output = ', '.join(mentions)
+            index = output.rfind(',')
+            output = output[:index] + ' and' + output[index+1:]
+            await ctx.send(output + " haven't responded")
 
     @commands.command()
     async def repo(self, ctx):
@@ -159,7 +184,7 @@ class listener(commands.Cog):
             elif str(payload.emoji) == "❌":
                 column_index = 7
             if column_index != -1:
-                SQL = self.computesql(self.DATABASE_POLL_TABLE, "update", str(payload.channel_id), "'" + str(payload.user_id) + "'", "'" + str(user.name) + "'", column, column_index, self.args)
+                SQL = self.computesql(self.DATABASE_POLL_TABLE, "add_user_selection", str(payload.channel_id), "'" + str(payload.user_id) + "'", "'" + str(user.name) + "'", column, column_index, self.args)
                 self.cur.execute(SQL)
                 self.conn.commit() #must commit to database
                 #await channel.send("executed update/insert SQL: " + SQL)
@@ -173,68 +198,70 @@ class listener(commands.Cog):
         #sql implementation
         column_index = -1
         column = ""
+        value = ""
         SQL = self.computesql(self.DATABASE_POLL_MESSAGE_ID_TABLE, "fetch_poll_message", "", "'" + str(payload.message_id) + "'", "'" + str(payload.channel_id) + "'", 0, 0, self.args)
         self.cur.execute(SQL)
         fetch = self.cur.fetchone()
         if int(fetch[0]) == int(payload.message_id) and int(fetch[1]) == int(payload.channel_id):
             if str(payload.emoji) == "1️⃣":
-                column_index = "FALSE"
+                value = "FALSE"
                 column = "monday"
             elif str(payload.emoji) == "2️⃣":
-                column_index = "FALSE"
+                value = "FALSE"
                 column = "tuesday"
             elif str(payload.emoji) == "3️⃣":
-                column_index = "FALSE"
+                value = "FALSE"
                 column = "wednesday"
             elif str(payload.emoji) == "4️⃣":
-                column_index = "FALSE"
+                value = "FALSE"
                 column = "thursday"
             elif str(payload.emoji) == "5️⃣":
-                column_index = "FALSE"
+                value = "FALSE"
                 column = "friday"
             elif str(payload.emoji) == "6️⃣":
-                column_index = "FALSE"
+                value = "FALSE"
                 column = "saturday"
             elif str(payload.emoji) == "7️⃣":
-                column_index = "FALSE"
+                value = "FALSE"
                 column = "sunday"
             elif str(payload.emoji) == "❌":
-                column_index = "FALSE"
+                value = "FALSE"
                 column = "unavailable"
             if column_index != -1:
-                SQL = self.computesql(self.DATABASE_POLL_TABLE, "update_false", "'" + str(payload.channel_id) + "'", "'" + str(payload.user_id) + "'", "'" + str(user.name) + "'", column, column_index, self.args)
+                user_id = "'" + str(payload.user_id) + "'"
+                channel_id = "'" + str(payload.channel_id) + "'"
+                username = "'" + str(user.name) + "'"
+                SQL = self.computesql(table=self.DATABASE_POLL_TABLE, action="remove_user_selection", value=value, user_id=user_id, channel_id=channel_id, username=username, column=column, args=self.args)
                 self.cur.execute(SQL)
-                self.conn.commit() #must commit to database
-                #await channel.send("executed delete SQL: " + SQL)
+                self.conn.commit()
+                SQL = self.computesql(table=self.DATABASE_POLL_TABLE, action="delete_user_if_no_reactions", user_id=user_id, channel_id=channel_id)
+                self.cur.execute(SQL)
+                self.conn.commit()
 
-    def computesql(self, table, action, channel_id, user_id, username, column, column_index, args):
+    def computesql(self, table="", action="", value="", user_id="", username="", channel_id="", column=-1, column_index=-1, args=[], message_id="", unavailable="FALSE"):
         SQL = ''
-        if action == "update":
+        if action == "add_user_selection":
             dup = args.copy()
             if column_index != 7:
                 dup[column_index] = "TRUE"
                 #you need to add channel_id in here later for multi-channel use
-                unavailable = "FALSE"
-                SQL = "INSERT INTO {table} (user_id, username, channel_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, unavailable) VALUES ({user_id}, {username}, {channel_id}, {args[0]}, {args[1]}, {args[2]}, {args[3]}, {args[4]}, {args[5]}, {args[6]}, {unavailable}) ON CONFLICT (user_id) DO UPDATE SET {column} = TRUE;".format(table=table, user_id=user_id, username=username, channel_id=channel_id, column=column, args=dup, unavailable=unavailable)
-            elif column_index == 7:
-                unavailable = "TRUE"
-                SQL = "INSERT INTO {table} (user_id, username, channel_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, unavailable) VALUES ({user_id}, {username}, {channel_id}, {args[0]}, {args[1]}, {args[2]}, {args[3]}, {args[4]}, {args[5]}, {args[6]}, {unavailable}) ON CONFLICT (user_id) DO UPDATE SET unavailable = TRUE;".format(table=table, user_id=user_id, username=username, channel_id=channel_id, column=column, args=dup, unavailable=unavailable)
-        elif action == "update_false":
-            SQL = "UPDATE {table} SET {column} = {column_index} WHERE user_id = {user_id} AND channel_id = {channel_id};".format(table=table, user_id=user_id, column=column, column_index=column_index, channel_id=channel_id)
-        elif action == "delete":
-            SQL = "DELETE FROM {table} WHERE user_id = {user_id};".format(table=table, user_id=user_id)
-        elif action == "check_user":
-            SQL = "SELECT CASE WHEN monday | tuesday | wednesday | thursday | friday | saturday | sunday = 0 THEN FALSE ELSE TRUE END as \"deletable\" FROM {table} WHERE user_id = {user_id};".format(table=table, user_id=user_id)
-        elif action == "fetch_users":
+                SQL = "INSERT INTO {table} (user_id, username, channel_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, unavailable) VALUES ({user_id}, {username}, {channel_id}, {args[0]}, {args[1]}, {args[2]}, {args[3]}, {args[4]}, {args[5]}, {args[6]}, {unavailable}) ON CONFLICT (user_id, channel_id) DO UPDATE SET {column} = {value};".format(table=table, value=value, user_id=user_id, username=username, channel_id=channel_id, column=column, args=dup, unavailable=unavailable)
+        elif action == "remove_user_selection":
+            SQL = "UPDATE {table} SET {column} = {value} WHERE user_id = {user_id} AND channel_id = {channel_id};".format(table=table, value=value, user_id=user_id, column=column, channel_id=channel_id)
+        elif action == "delete_user_if_no_reactions":
+            SQL = "IF ((SELECT COUNT(user_id) FROM {table} WHERE (monday OR tuesday OR wednesday OR thursday OR friday OR saturday OR sunday OR unavailable) = FALSE AND user_id = {user_id} AND channel_id = {channel_id}) > 0) THEN DELETE FROM {table} WHERE user_id = {user_id} AND channel_id = {channel_id} END IF;".format(table=table, user_id=user_id, channel_id=channel_id)
+        elif action == "fetch_all_users":
+            SQL = "SELECT user_id, username FROM {table} WHERE channel_id = {channel_id};".format(table=table, channel_id=channel_id)
+        elif action == "fetch_available_users":
             SQL = "SELECT user_id, username FROM {table} WHERE {column} = TRUE AND channel_id = {channel_id} AND unavailable = FALSE;".format(table=table, channel_id=channel_id, column=column)
         elif action == "delete_poll_message":
-            SQL = "DELETE FROM {table} WHERE poll_message_id = {user_id} AND channel_id = {username};".format(table=table, user_id=user_id, username=username)
-        elif action == "check_poll_message":
-            SQL = "SELECT poll_message_id, channel_id FROM {table} WHERE channel_id = {username};".format(table=table, username=username)
+            SQL = "DELETE FROM {table} WHERE poll_message_id = {message_id} AND channel_id = {channel_id};".format(table=table, message_id=message_id, channel_id=channel_id)
+        elif action == "check_poll_message_exists":
+            SQL = "SELECT poll_message_id, channel_id FROM {table} WHERE channel_id = {channel_id};".format(table=table, channel_id=channel_id)
         elif action == "fetch_poll_message":
-            SQL = "SELECT poll_message_id, channel_id FROM {table} WHERE poll_message_id = {user_id} AND channel_id = {username};".format(table=table, user_id=user_id, username=username)
+            SQL = "SELECT poll_message_id, channel_id FROM {table} WHERE poll_message_id = {message_id} AND channel_id = {channel_id};".format(table=table, message_id=message_id, channel_id=channel_id)
         elif action == "set_poll_message":
-            SQL = "INSERT INTO {table} (poll_message_id, channel_id) VALUES ({user_id}, {username});".format(table=table, user_id=user_id, username=username)
+            SQL = "INSERT INTO {table} (poll_message_id, channel_id) VALUES ({message_id}, {channel_id});".format(table=table, message_id=message_id, channel_id=channel_id)
         return SQL
 
 class BotClass():
